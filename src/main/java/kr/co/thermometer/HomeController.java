35386ffc51fe7a030bed9ccfd02bbbfa9964e06b
yy9320 <pyenkr93@gmail.com>
2020-01-15 23:57:31 +0900

    id 별로 출력해서 다른 각자의 id로 가지고 있는 temperature 결롸값 가져오기

--- src/main/java/kr/co/thermometer/HomeController.java
+++ src/main/java/kr/co/thermometer/HomeController.java
@@ -5,13 +5,16 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.JSpinner.ListEditor;
@@ -48,15 +51,6 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-//		String formattedDate = dateFormat.format(date);
-//		List<Temperature> listTemperature = new ArrayList<Temperature>();
-//		listTemperature = callJpa();
-//		int size = listTemperature.size();
-//		model.addAttribute("serverTime", formattedDate );
-//		model.addAttribute("temperateDate", listTemperature.get(0).getTemperature());
-//		model.addAttribute("userId", listTemperature.get(0).getId());
-//		model.addAttribute("size", size);
-//		model.addAttribute("temperatureList", listTemperature);
 		return "home";
 	}
 
@@ -68,34 +62,58 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		List<Temperature> listTemperature = new ArrayList<Temperature>();
-		listTemperature = callJpa();
+		// 우선 member를 중복되지 않게 구한다.  얘는 무조건 중복 없음
+		List<Member> listMember = new ArrayList<Member>();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("function", "selectMember");
+		listMember = (List<Member>) callJpa(map);
 		int size = listTemperature.size();
 		
 		JSONArray jsonArr =  new JSONArray();
-		for (int i=0; i<listTemperature.size(); i++){
-			JSONObject result = new JSONObject();
-			result.put("id", listTemperature.get(i).getId().toString());
-			result.put("date", listTemperature.get(i).getDate().toString());
-			result.put("temperature", listTemperature.get(i).getTemperature().toString());
-			jsonArr.add(result);
+		
+		// 중복되지 않은 멤버들로 일주일 양을 구한다. 
+		// member를 for문 돌면서 일주일치 모두를 구한뒤 map list 형태로 구한다. 
+		// 일주일의 날짜 데이터를 가지고 가서 매핑 시켜준다. vs 일주일의 데이터를 없으면 없게 넣어줘서 가져간다.?
+		
+		for (int i=0; i<listMember.size(); i++){
+			JSONObject data = new JSONObject();
+			String id = listMember.get(i).getId();
+			Map<String, String> param = new HashMap<String, String>();
+			param.put("people", "more");
+			
+			param.put("id", id);
+			param.put("function", "selectTemperature");
+			// list<temperature>
+			List<Temperature> x =  (List<Temperature>) callJpa(param);
+			jsonArr.add(x);
 		}
+			
+	
+			
+//			Map<String, List<Map<String,String>>> list = new HashMap<String, List<Map<String,String>>>();
+//			
+//			Map<String, List<String>> listColumn = new Map<String, List<String>>();
+//			
+//			result.put("id", listTemperature.get(i).getId().toString());
+//			result.put("date", listTemperature.get(i).getDate().toString());
+//			result.put("temperature", listTemperature.get(i).getTemperature().toString());
 		
 		JSONObject jsonObj = new JSONObject();
-		System.out.println(jsonArr);
 		jsonObj.put("data", jsonArr);
 		return jsonObj;
 	}
 	
 	
-	public static List<Temperature> callJpa() {
+	public static Object callJpa(Map<String, String> map) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("thermometer");
 		EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
 		EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
-		List<Temperature> listTemperature = new ArrayList<Temperature>();
+		Object result = new Object();
 		
         try {
             tx.begin(); //트랜잭션 시작
-            listTemperature = selectTemperature(em);  //비즈니스 로직
+//            listTemperature = selectTemperature(em);  //비즈니스 로직
+            result = logic(map, em);
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -106,7 +124,16 @@ public class HomeController {
         }
 
         emf.close(); //엔티티 매니저 팩토리 종료
-        return listTemperature;
+        return result;
+	}
+	
+	public static Object logic(Map<String,String> param, EntityManager em) {
+		String function = (String) param.get("function");
+		if (function.equals("selectMember")) {
+			return selectMember(em);
+		} else {
+			return selectTemperature(em, param );
+		}
 	}
 	
 	public static void insertMember(EntityManager em) {
@@ -118,7 +145,7 @@ public class HomeController {
 		em.persist(member);
 	}
 	
-	public static void selectMember(EntityManager em) {
+	public static List<Member> selectMember(EntityManager em) {
 		String id = "id1";
 		//한 건 조회
         Member findMember = em.find(Member.class, id);
@@ -128,10 +155,11 @@ public class HomeController {
         List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
         System.out.println("members.size=" + members.size());
 
+        return members;
 	}
 
-	public static void insertTemperature(EntityManager em) {
-		String id = "id1";
+	public static void insertTemperature(EntityManager em, Map<String, String> param) {
+		String id = param.get("id");
 		Temperature temperature = new Temperature();
 		Date date = new Date();
 		
@@ -144,17 +172,34 @@ public class HomeController {
 		em.persist(temperature);
 		
 	}
-	
-	public static List<Temperature> selectTemperature(EntityManager em) {
-		String id = "id1";
+	/**
+	 * 
+	 * @param em
+	 * @param param
+	 *  id - 찾으려는 id의 온도 리스트 
+	 *  people - one or more one이면 한사람만, more이면 여러사람 
+	 * @return
+	 */
+	public static List<Temperature> selectTemperature(EntityManager em, Map<String, String> param) {
+		String id = param.get("id");
 		//한 건 조회
-		Temperature findTemperature = em.find(Temperature.class, id);
-		System.out.println("findTemperature=" + findTemperature.getTemperature() + ", Date=" + findTemperature.getDate());
-		
-		//목록 조회
-		List<Temperature> temperature = em.createQuery("select m from Temperature m", Temperature.class).getResultList();
-		System.out.println("temperature.size=" + temperature.size());
-		
+		List<Temperature> temperature = new ArrayList<Temperature>();
+		if (param.get("people").equals("one")) {
+			Temperature oneData = em.find(Temperature.class, id);
+			temperature = new ArrayList<Temperature>();
+			temperature.add(oneData);
+			System.out.println("findTemperature=" + temperature.get(0).getTemperature() 
+					+ ", Date=" + temperature.get(0).getDate());
+		} else {
+			//목록 조회
+			TypedQuery<Temperature> query = em.createQuery("" + "select m from Temperature m where m.id = :id", Temperature.class);
+			query.setParameter("id", id);
+			temperature = query.getResultList();
+			
+//			temperature = em.createQuery("select m from Temperature m m.id = :id", Temperature.class).getResultList();
+			
+			System.out.println("temperature.size=" + temperature.size());
+		}
 		return temperature;
 	}
 }
